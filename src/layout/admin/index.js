3e6ba21802a3ef6793af389d3e61db154d8a327c
Anvesh Balaji <Anvesh.Babu.-ND@disney.com>
2022-09-08 08:28:42 +0530

    path changes

--- src/layout/admin/index.js
+++ src/layout/admin/index.js
@@ -3,6 +3,7 @@ import './primary.scss';
 import { Header } from './header'
 import { Sidebar } from './sideBar'
 import { CURRENT_USER } from '../../services/constants'
+import { history } from '../../helpers'
 import { getStorage, isEmpty } from '../../services/helperFunctions'
 import { Outlet } from "react-router-dom";
 export const Adminlayout = ({ children }) => {
@@ -10,9 +11,13 @@ export const Adminlayout = ({ children }) => {
     const [isAuth, setIsAuth] = useState(false);
 
     useEffect(() => {
+        const pathname = window.location.hash;
         if (!!getStorage(CURRENT_USER)) {
             let isAuth = !isEmpty(JSON.parse(getStorage(CURRENT_USER)));
             setIsAuth(isAuth)
+            if(!pathname){
+                history.push('/dashboard')
+            }
         }
 
     }, [])
